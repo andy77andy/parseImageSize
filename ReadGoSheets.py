2ff4e89c7db0feb1aa9d2cac042482b63bc24949
andy <andy77andy@ukr,net>
2024-04-17 15:40:09 +0300

    Deleted redundant files

--- ReadGoSheets.py
+++
@@ -1,9 +0,0 @@
-import gspread
-import requests
-from oauth2client.service_account import ServiceAccountCredentials
-
-scopes = {
-    'https://www.googleapis.com/auth/spreadsheets',
-    'https://www.googleapis.com/auth/drive'
-}
-
